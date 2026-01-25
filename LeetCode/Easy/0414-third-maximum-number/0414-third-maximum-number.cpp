f2be1599812953e1388e9ac819f589d2f310281f
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-01-25 21:31:11 +0530

    0414-third-maximum-number

--- LeetCode/Easy/0414-third-maximum-number/0414-third-maximum-number.cpp
+++ LeetCode/Easy/0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int ans = INT_MIN;
-        set<int> s;
-        for(auto i: nums){
-            s.insert(i);
-            if(s.size()>3){
-                s.erase(s.begin());
+        // int ans = INT_MIN;
+        // set<int> s;
+        // for(auto i: nums){
+        //     s.insert(i);
+        //     if(s.size()>3){
+        //         s.erase(s.begin());
+        //     }
+        //     // cout<<i<<*s.begin()<<*s.rbegin()<<endl;
+        // }
+        // // cout<<*s.begin()<<*s.rbegin()<<s.size();
+        // return s.size() < 3 ? *s.rbegin() : *s.begin();
+
+
+        // use long as max and min can include INT_MAX or INT_MIN
+        long long int first = LLONG_MIN;
+        long long int second = first;
+        long long int third = first;
+
+        for(auto i:nums){
+            if(i == first || i==second || i == third)
+                continue;
+            if(i>first){
+                third = second;
+                second = first;
+                first = i;
             }
-            // cout<<i<<*s.begin()<<*s.rbegin()<<endl;
+            else if(i > second){
+                third = second;
+                second = i;
+            }
+            else if(i> third){
+                third = i;
+            }
         }
-        // cout<<*s.begin()<<*s.rbegin()<<s.size();
-        return s.size() < 3 ? *s.rbegin() : *s.begin();
+        // cout<<first<<" "<<second<<" "<<third<<endl;
+        return third != LLONG_MIN ? third : first;
     }
 };
