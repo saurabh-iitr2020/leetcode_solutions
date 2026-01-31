0b77da626ead03f968f3837e9246b972082cdf7b
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-01-31 16:07:03 +0530

    0744-find-smallest-letter-greater-than-target

--- LeetCode/Easy/0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ LeetCode/Easy/0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char ans = letters[0];
-        for(auto i: letters){
-            if(i>target){
-                ans = i;
-                break;
+        // Time: O(n)
+        // char ans = letters[0];
+        // for (auto i : letters) {
+        //     if (i > target) {
+        //         ans = i;
+        //         break;
+        //     }
+        // }
+        // return ans;
+
+        // arr is sorted then binary search can be used
+        // Time: O(log n)
+        int l = 0, r = letters.size()-1;
+        if(target >= letters[r]){
+            return letters[0];
+        }
+        while(l<=r){
+            int m = l + (r-l)/2;
+            if(letters[m]>target){
+                r=m-1;
+            }else{
+                l=m+1;
             }
         }
-        return ans;
+        return letters[l];
+
+
     }
 };
