274b55e9345250252bf7cfd126ab5f582dfee3a3
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-08 15:04:46 +0530

    1653-minimum-deletions-to-make-string-balanced

--- LeetCode/Medium/1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ LeetCode/Medium/1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -23,5 +23,36 @@ public:
         }
         return ans;
     }
-    int minimumDeletions(string s) { return greedy(s); }
+
+    // for each index
+    // precaculate no-A on right side and no-B on left side
+    // update ans = min(ans, no-A[i] + no-B[i])
+    // considering we are removing all left B and right A at each index
+    // ans will min of all these ops
+    int splitParts(string s) {
+        int n = s.size();
+        vector<int> noOfARight(n, 0);
+        int totalA = 0;
+        for (int i = n - 1; i >= 1; i--) {
+            noOfARight[i] = totalA;
+            if (s[i] == 'a') {
+                totalA++;
+            }
+        }
+        noOfARight[0] = totalA;
+
+        int ans = totalA, leftB = 0;
+        for (int i = 0; i < n; i++) {
+            int rightA = noOfARight[i];
+            ans = min(ans, rightA + leftB);
+            if(s[i]=='b')
+                leftB++;
+            // cout<<i<<leftB<<rightA<<ans<<endl;
+        }
+        return ans;
+    }
+    int minimumDeletions(string s) {
+        // return greedy(s);
+        return splitParts(s);
+    }
 };
