5a4d0c882fda14953022169cf7be68a3c98fddb4
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-09 20:40:57 +0530

    1382-balance-a-binary-search-tree

---
+++ LeetCode/Medium/1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preOrder(TreeNode* root, vector<int>& v) {
+        if (!root) {
+            return;
+        }
+        v.push_back(root->val);
+        preOrder(root->left, v);
+        preOrder(root->right, v);
+    }
+    TreeNode* buildTree(vector<int>& v, int start, int end) {
+        if (start > end)
+            return NULL;
+
+        int mid = start + (end - start) / 2;
+        // cout << mid << v[mid] << endl;
+        TreeNode* root = new TreeNode(v[mid]);
+        root->left = buildTree(v, start, mid - 1);
+        root->right = buildTree(v, mid + 1, end);
+        return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> nodes;
+        preOrder(root, nodes);
+        sort(nodes.begin(), nodes.end());
+        TreeNode* newRoot = buildTree(nodes, 0, nodes.size() - 1);
+        return newRoot;
+    }
+};
